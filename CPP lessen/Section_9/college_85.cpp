0affc0a4fee50b2e6d9908e311db36fbe69c75da
reddot-belgium <45652414+reddot-belgium@users.noreply.github.com>
2019-01-09 16:44:26 +0100

    college 85

--- CPP lessen/Section_9/college_85.cpp
+++ CPP lessen/Section_9/college_85.cpp
@@ -1,3 +1,6 @@
+// Sectie 9
+// college 85
+
 #include <iostream>
 
 using namespace std;
@@ -16,14 +19,41 @@ int main()
     // cout << "Blastoff!" << endl;
 
     // case2
-    int num {};
-    cout << "Ente a positive integer to count up to: ";
-    cin >> num;
+    // int num {};
+    // cout << "Ente a positive integer to count up to: ";
+    // cin >> num;
 
-    int i {1};
-    while (num >= i){
-        cout << i << endl;
-        i++;
+    // int i {1};
+    // while (num >= i){
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    // case 3
+    // int number {};
+
+    // cout << "Enter a integer less than 100: ";
+    // cin >> number;
+
+    // while (number >= 100){
+    //     cout << "Enter an integer less than 100: ";
+    //     cin >> number;
+    // }
+    // cout << "Tanks, your number is: " << number << endl;
+
+    // case 4
+    bool done (false);
+    int number {0};
+
+    while (!done){
+        cout << "Enter an integer between 1 and 5: ";
+        cin >> number;
+        if (number <= 1 || number >=5)
+            cout << "Out of range, try again" << endl;
+        else {
+            cout << "Thanks!" << endl;
+            done = true;
+        }  
     }
 
     return 0;
