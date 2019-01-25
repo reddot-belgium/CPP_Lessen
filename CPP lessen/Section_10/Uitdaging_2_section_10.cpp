b6e6a559f4fdcb57fc99693720150019115677a4
reddot-belgium <45652414+reddot-belgium@users.noreply.github.com>
2019-01-25 14:48:07 +0100

    Spacies proberen toe te voegen

--- CPP lessen/Section_10/Uitdaging_2_section_10.cpp
+++ CPP lessen/Section_10/Uitdaging_2_section_10.cpp
@@ -10,11 +10,17 @@ int main(){
     cout << "input a string. So a can make a piramide of it!: ";
     getline(cin, input_user);
 
-    for (size_t i {1}; i < input_user.length(); ++i){
+    for (size_t i {0}; i < input_user.length(); ++i){
+        
+        int spacies = input_user.length() - i;
+        string spacer (spacies, ' ');
+
        for(size_t x {0}; x <= i; ++x){
-           cout << input_user.substr(x,1);
+           cout << spacer << input_user.substr(x,1);
        }
+
        cout << endl;
+
     }
 
     cout << endl;
