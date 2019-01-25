80f9ea50aec31bea25040517e1daf3cdfb14a49d
reddot-belgium <45652414+reddot-belgium@users.noreply.github.com>
2019-01-25 13:37:28 +0100

    for loop poging 1

--- CPP lessen/Section_10/Uitdaging_2_section_10.cpp
+++ CPP lessen/Section_10/Uitdaging_2_section_10.cpp
@@ -10,7 +10,12 @@ int main(){
     cout << "input a string. So a can make a piramide of it!: ";
     getline(cin, input_user);
 
-    cout << input_user << endl;
+    for (size_t i {1}; i < input_user.length(); ++i){
+       for(size_t x {0}; x <= i; ++x){
+           cout << input_user.substr(x,1);
+       }
+       cout << endl;
+    }
 
     cout << endl;
     return 0;
