32637566d4becc2187bb130b0007f55a0be036ee
reddot-belgium <45652414+reddot-belgium@users.noreply.github.com>
2019-01-25 12:32:51 +0100

    Sectrion 10 challenge

---
+++ CPP lessen/Section_10/Challenge_section_10.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    string key {"QWERTYUIOPASDFGHJKLZXCVBNMmnbvcxzlkjhgfdsapoiuytrewq"};
+
+    string bericht{};
+    
+    cout << "geef in wat je wil encrypted: ";
+    getline(cin, bericht);
+
+    string scrambel_bericht{};
+
+    cout << "Encrypting message...";
+
+    for (char c: bericht){
+        size_t position = alphabet.find(c);
+        if (position != string::npos){
+            char new_char{key.at(position)};
+            scrambel_bericht += new_char;
+        } else {
+            scrambel_bericht += c;
+        }
+    }
+
+    cout << "\nEncrypted message: " << scrambel_bericht << endl;
+
+    string unscrambled_bericht{};
+
+    cout << "Encrypting message...";
+
+    for (char c: scrambel_bericht){
+        size_t position = key.find(c);
+        if (position != string::npos){
+            char new_char{alphabet.at(position)};
+            unscrambled_bericht += new_char;
+        } else {
+            unscrambled_bericht += c;
+        }
+    }
+
+    cout << "\nUn Encrypted message: " << unscrambled_bericht << endl;
+
+    
+
+    cout << endl;
+    return 0;
+}
