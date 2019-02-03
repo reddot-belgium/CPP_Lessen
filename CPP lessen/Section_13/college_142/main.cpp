943b33f0e73cc7aa6a9536d7a92d2c35d8c2b973
reddot-belgium <45652414+reddot-belgium@users.noreply.github.com>
2019-02-03 16:16:54 +0100

    Start van College 142 Default Constructors

--- CPP lessen/Section_13/college_142/main.cpp
+++ CPP lessen/Section_13/college_142/main.cpp
@@ -1,4 +1,22 @@
+/* Section 13*/
+/* Default Constructors */
+/* -------------------- */
+
 #include <iostream>
+#include <string>
+
+class Player
+{
+private:
+	std::string name;
+	int health;
+	int xp;
+
+public:
+	void set_name(std::string name_val) {
+		name = name_val;
+	}
+};
 
 int main() {
 
