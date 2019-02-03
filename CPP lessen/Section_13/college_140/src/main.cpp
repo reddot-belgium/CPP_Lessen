81494fc2b49b1531bfcbcaeed0686d63a41dbdf2
reddot-belgium <45652414+reddot-belgium@users.noreply.github.com>
2019-02-03 08:54:01 +0100

    Map met voorbeelden leeraar toegevoegd

--- CPP lessen/Section_13/college_140/src/main.cpp
+++ CPP lessen/Section_13/college_140/src/main.cpp
@@ -1,9 +1,73 @@
+// Section 13
+// Implementing member methods 1 
 #include <iostream>
+#include <string>
 
+
+class Account {
+private:
+    // attributes
+    std::string name;
+    double balance;
+    
+public:
+    /* methods */
+    /* declared inline */
+    void set_balance(double bal) { balance = bal; }
+    double get_balance() { return balance; }
+    
+    /* methods will be declared outside the class declaration */
+    void set_name(std::string n);
+    std::string get_name();
+    
+    bool deposit(double amount);
+    bool withdraw(double amount);
+};
+
+void Account::set_name(std::string n) {
+    name = n;
+}
+
+std::string Account::get_name() {
+    return name;
+}
+
+bool Account::deposit(double amount) {
+    // if verify amount
+    balance += amount;
+    return true;
+}
+
+bool Account::withdraw(double amount) {
+    if (balance-amount >= 0) {
+        balance -= amount;
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 int main() {
-
-    int test {5};
-    std::cout << "dit is een test project: " << test << std::endl;
+    Account frank_account;
+    frank_account.set_name("Frank's account");
+    frank_account.set_balance(1000.0);
+    
+    if (frank_account.deposit(200.0))
+        std::cout << "Deposit OK" << std::endl;
+    else 
+        std::cout << "Deposit Not allowed" << std::endl;
+        
+    if (frank_account.withdraw(500.0))
+        std::cout << "Withdrawal OK" << std::endl;
+    else
+        std::cout << "Not sufficient funds" << std::endl;
+        
+    if (frank_account.withdraw(1500.0))
+        std::cout << "Withdraw OK" << std::endl;
+    else
+        std::cout << "Not sufficient funds" << std::endl;
+    
+    return 0;
 }
+
