086920493cd47e32f5a230e5cab784c128471623
reddot-belgium <45652414+reddot-belgium@users.noreply.github.com>
2019-02-04 21:38:43 +0100

    enide college 147
    copy constructor

--- CPP lessen/Section_13/college_147/main.cpp
+++ CPP lessen/Section_13/college_147/main.cpp
@@ -15,19 +15,39 @@ public:
 	std::string get_name() { return name; }
 	int get_health() { return health; }
 	int get_xp() { return xp; }
+
 	Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
+
+	// Copy constructor
+	Player(const Player &source);
+
+	// Destructor
+	~Player() { cout << "Destructor call for: " << name << endl; }
 };
 
 
 
 Player::Player(std::string name_val, int health_val, int xp_val)
 	: name{ name_val }, health{ health_val }, xp{ xp_val } {
-	cout << "Three-args constructor" << endl;
+	std::cout << "Three-args constructor" << std::endl;
+}
+
+Player::Player(const Player &source)
+	: Player{source.name, source.health, source.xp} {
+	std::cout << "Copy constructor - made copy of: " << source.name << std::endl;
+	}
+
+void display_player(Player p) {
+	std::cout << "Name: " << p.get_name() << std::endl;
+	std::cout << "Health: " << p.get_health() << std::endl;
+	std::cout << "XP: " << p.get_xp() << endl;
 }
 
 int main() {
 
 	Player empty;
+	Player my_new_object(empty);
+	display_player(empty);
 	Player frank{ "Frank" };
 	Player Hero{ "Hero",120 };
 	Player villain{ "Villain", 100, 55 };
